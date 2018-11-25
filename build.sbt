0eec05f25a1bb14afd7e9c161512c2aabc749fbf
Ivan Ehreshi <IvanEhreshi@gmail.com>
2018-11-25 15:56:06 +0200

    Implement Lexer parser with logging and idle state management

--- build.sbt
+++ build.sbt
@@ -6,3 +6,7 @@ scalaVersion := "2.12.1"
 
 libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.26"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.11.1"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.11.1"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-api" % "2.11.1"
--- src/main/scala/automata/Lexer.scala
+++ src/main/scala/automata/Lexer.scala
@@ -1,26 +1,54 @@
 package automata
 
+import com.typesafe.scalalogging.StrictLogging
+
+import scala.collection.mutable
 import scalaz.{-\/, \/, \/-}
 
-object Lexer {
+object Lexer extends StrictLogging {
+
   def parse[M](expression: String, automata: LexerAutomata[M]): String \/ List[String] = {
     var idx = 0
     val tokens = List.newBuilder[String]
     val symbols = expression + '\0'
 
+    var curr = automata
+    logger.debug("-> " + curr.state)
+
     while (idx < symbols.length()) {
-      val next = automata << expression(idx)
+      curr = curr << symbols(idx)
+      logger.debug("-> " + curr.state)
 
-      if (next.error.isDefined)
-        return -\/(next.error.get)
+      val oldState = curr.state
 
-      if (next.token.isDefined)
-        tokens += next.token.get
+      if (curr.error.isDefined)
+        return -\/(curr.error.get)
+
+      appendTokenIfExists(tokens, curr)
+
+      curr = curr.idle()
+
+      logIdleTransition(curr, oldState)
+
+      if (curr.error.isDefined)
+        return -\/(curr.error.get)
+
+      appendTokenIfExists(tokens, curr)
 
       idx += 1
     }
 
 
     \/-(tokens.result())
+  }
+
+  private def logIdleTransition[M](current: LexerAutomata[M], oldState: String) = {
+    if (current.state != oldState)
+      logger.debug("->>" + current.state + " " + (current match { case r: RunningAutomata[M] => r.memory; case _ => ""}))
+  }
+
+  private def appendTokenIfExists[M](tokens: mutable.Builder[String, List[String]], curr: LexerAutomata[M]) = {
+    if (curr.token.isDefined)
+      tokens += curr.token.get
   }
 }
