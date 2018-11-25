5407d26c6d6f6687ad3b25309def136dee01d229
Ivan Ehreshi <IvanEhreshi@gmail.com>
2018-11-25 15:39:32 +0200

    Make the usage of Action and Condition more convenient

--- src/main/scala/automata/Action.scala
+++ src/main/scala/automata/Action.scala
@@ -3,7 +3,11 @@ package automata
 case class Action[-M](pushToken: Boolean, accumulate: Boolean, nextMemory: M => Any) {
   def +[Z <: M](that: Action[Z]): Action[M] = Action(pushToken = this.pushToken || that.pushToken,
     accumulate = this.accumulate || that.accumulate,
-    nextMemory = nextMemory andThen { any => any.asInstanceOf[M] })
+    nextMemory = nextMemory andThen { any => that.nextMemory(any.asInstanceOf[Z]) })
+}
+
+object Action {
+  def mutateMemory[M](nextMemory: M => Any): Action[M] = Action(false, false, nextMemory)
 }
 
 object NoAction extends Action[Any](false, false, identity)
--- src/main/scala/automata/Condition.scala
+++ src/main/scala/automata/Condition.scala
@@ -4,10 +4,18 @@ sealed abstract class Condition[-M] {
   def accepts(symbol: Symbol, memory: M, accumulator: String): Boolean
   protected val priority: Int = 0
 
+  def &&[Z <: M](that: Condition[Z]): Condition[Z]
+    = ExprCondition({ (symb, mem, acc) => this.accepts(symb, mem, acc) && that.accepts(symb, mem, acc)})
+  def neg(): Condition[M] = ExprCondition({ (symb, mem, acc) => !this.accepts(symb, mem, acc)})
 }
 
 object Condition {
   def ordering[M]: Ordering[Condition[M]] = (x: Condition[M], y: Condition[M]) => x.priority - y.priority
+  def anyChar(chars: Char*): Condition[Any] = CharCondition({ chars contains _})
+  def whenMemory[M](cond: M => Boolean): Condition[M] = ExprCondition({ (_, mem, _) => cond(mem) })
+  def whenAlpha: Condition[Any] = CharCondition({ _.isLetter })
+  val whenTerminal: Condition[Any] = Match(Symbol.Terminal)
+  val whenDigit: Condition[Any] = CharCondition({ _.isDigit })
 
 }
 
@@ -19,9 +27,15 @@ case class ExprCondition[M](expr: (Symbol, M, String) => Boolean) extends Condit
   override def accepts(symbol: Symbol, memory: M, accumulator: String): Boolean = expr(symbol, memory, accumulator)
 }
 
+case class CharCondition(expr: Char => Boolean) extends Condition[Any] {
+  override def accepts(symbol: Symbol, memory: Any, accumulator: String): Boolean = symbol match {
+    case Symbol.Char(c) => expr(c)
+    case _ => false
+  }
+}
 
 private[automata] object Else extends Condition[Any] {
-  override def accepts(char: Symbol, memory: Any, accumulator: String): Boolean = true
+  override def accepts(char: Symbol, memory: Any, accumulator: String): Boolean = char != Symbol.None
   override protected val priority: Int = 1
 }
 
